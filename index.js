cb03b3c873bb8cd5b18f3b1ef7d4920a8a773294
Chris Cardle <cdcardle@gmail.com>
2018-10-11 18:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const returnFirstTwoDrivers = (drivers => drivers.slice(0,2));
 
 const returnLastTwoDrivers = (drivers => drivers.slice(-2));
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(integer) {
   return (fare => (fare * integer));
