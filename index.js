7274bb6c85ffdbaeb65268ae41547cb9bb9b520d
Chris Cardle <cdcardle@gmail.com>
2018-10-11 18:32:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 
 
-const returnFirstTwoDrivers = (drivers =>drivers.slice(0,2));
+const returnFirstTwoDrivers = (drivers => drivers.slice(0,2));
+
+const returnLastTwoDrivers = (drivers => drivers.slice(-2));
