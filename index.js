7ebb56f511b97080f91ff37affa26157bace3fe1
Chris Cardle <cdcardle@gmail.com>
2018-10-11 19:05:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(drivers, function) {
+function fetchSpecifiedDrivers = function(drivers, function) {
   return function;
 }
